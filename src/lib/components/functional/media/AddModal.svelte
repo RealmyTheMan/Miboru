72da72239a4d89bf28245cdee9bea4ed1954922c
RealmyTheMan <163438634+RealmyTheMan@users.noreply.github.com>
2025-05-07 20:18:45 +0200

    drag functionality for add modal

--- src/lib/components/functional/media/AddModal.svelte
+++ src/lib/components/functional/media/AddModal.svelte
@@ -3,12 +3,15 @@
   import Modal from "$lib/components/adaptable/Modal.svelte";
   import { addMedia } from "$lib/src/api/add";
   import { goto } from "$app/navigation";
+  import clsx from "clsx";
 
   interface Props {
     active: boolean;
   }
 
   let { active = $bindable() }: Props = $props();
+
+  let isDraggingOver = $state(false);
 
   async function onClickUploader() {
     const input = document.createElement("input");
@@ -26,6 +29,15 @@
     input.click();
   }
 
+  function onDropFile(
+    e: DragEvent & { currentTarget: EventTarget & HTMLButtonElement },
+  ) {
+    e.preventDefault();
+    const files = e.dataTransfer?.files;
+    if (!files || !files[0]) return;
+    handleFileUpload(files[0]);
+  }
+
   async function handleFileUpload(file: File) {
     const { id } = await addMedia({ file, automaticTitle: file.name });
 
@@ -36,13 +48,21 @@
 
 <Modal bind:active>
   <div class="h-96">
-    <div
-      class="hover:bg-button flex h-full w-full items-center justify-center p-2
-transition-all active:opacity-85"
-    >
+    <div class="flex h-full w-full items-center justify-center p-2">
       <button
-        class="flex w-3/4 cursor-pointer flex-col items-center justify-center text-center"
-        onclick={() => onClickUploader()}
+        class={clsx(
+          "hover:bg-button flex w-3/4 cursor-pointer flex-col items-center justify-center rounded-lg p-8 text-center transition-all active:opacity-85",
+          {
+            "bg-button": isDraggingOver,
+          },
+        )}
+        ondragover={() => (isDraggingOver = true)}
+        ondragleave={() => (isDraggingOver = false)}
+        ondrop={(e) => {
+          isDraggingOver = false;
+          onDropFile(e);
+        }}
+        onclick={onClickUploader}
       >
         <div class="text-accent-red text-6xl"><UploadIcon /></div>
 
