6d9de08cb2a7318f7512ffe4b90c60b06b2fe7e9
Realmy <163438634+RealmyTheMan@users.noreply.github.com>
2025-05-08 19:59:33 +0200

    add *something* on the settings page

---
+++ src/routes/(main)/settings/+page.svelte
@@ -0,0 +1,9 @@
+<div class="flex h-full flex-col items-center justify-center text-center">
+  <p class="font-semibold">no settings exist yet</p>
+  <p class="mt-2">
+    ¿desearías aportar en añadirlos mediante <a
+      href="https://github.com/RealmyTheMan/Miboru"
+      class="text-accent-red font-semibold hover:underline">github</a
+    >? 🥺
+  </p>
+</div>
